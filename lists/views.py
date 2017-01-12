71e998ce9ba5ae510321128139a3e61af7ad2382
farhad <farhad@dohatec.com.bd>
2017-01-11 16:46:18 -0800

    Basic view returns minimal HTML

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-
-def home_page():
-    pass
+def home_page(reqest):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
 # Create your views here.
